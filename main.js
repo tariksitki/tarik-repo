ae477a8cd761e8c121fcff5207acfa1a09e565d7
tariksitki <tariksitki0119@gmail.com>
2020-04-20 10:26:56 +0200

    benim ilk calismalarim

---
+++ main.js
@@ -0,0 +1,22 @@
+
+function elma() { alert("(tote 74)  (Fälle 4297)");
+console.log("Hamburg button a tikladiniz" ) }
+
+function gbutton() { alert("(tote 4338) (Fälle 140780 )");
+console.log("Genel button a tikladiniz")   }
+
+function bbutton() { alert("(tote 92) ( Fälle 5187)");
+console.log("Berlin Butona Tikladiniz")}
+
+function nordbutton() {alert("(tote 862) (Fälle 29155)");
+console.log("Nordrhein Westfalen Butona Tikladiniz")}
+
+function hesbutton() {alert("(tote 230) (Fälle 7108)");
+console.log("Hessen butona tikladiniz")}
+
+function babuton() {alert("(tote 1256) ( Fälle 37407)");
+console.log("Bayern butona tikladiniz")}
+
+function badbutton() {alert("(tote 952) ( Fälle 27710");
+console.log("Baden Württemberg butona tikladiniz")}
+
